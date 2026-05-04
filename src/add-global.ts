c58f78ec511ed50cc434e4e8cffb1ae17d0591a2
Smarthard <th3smartchan@gmail.com>
2026-05-04 18:38:04 +0300

    add swagger

--- src/add-global.ts
+++ src/add-global.ts
@@ -4,12 +4,20 @@ import {
     ValidationPipe,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { readFile } from 'fs/promises';
+import yaml from 'yaml';
+import swagger from 'swagger-ui-express';
 
-export function addGlobal(app: INestApplication<any>) {
+export async function addGlobal(app: INestApplication<any>) {
+    const openapiFile = await readFile('openapi.yaml', { encoding: 'utf-8' });
+    const openapi = yaml.parse(openapiFile);
+
     app.useGlobalPipes(new ValidationPipe({ transform: true }));
     app.useGlobalInterceptors(
         new ClassSerializerInterceptor(app.get(Reflector), {
             excludeExtraneousValues: true,
         }),
     );
+
+    app.use('/docs', swagger.serve, swagger.setup(openapi, { customSiteTitle: 'Shikicinema API v1' }));
 }
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { addGlobal } from './add-global';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    addGlobal(app);
+    await addGlobal(app);
 
     await app.listen(process.env.SHIKICINEMA_API_V1_PORT ?? 8603);
 }
