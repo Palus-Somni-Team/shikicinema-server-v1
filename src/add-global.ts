7db99805eb586532fcd22049c042bda963a76f97
Smarthard <th3smartchan@gmail.com>
2026-05-05 07:47:39 +0300

    silence logging in tests

--- src/add-global.ts
+++ src/add-global.ts
@@ -11,6 +11,7 @@ import morgan from 'morgan';
 
 export async function addGlobal(app: INestApplication<any>) {
     const isProduction = process.env.NODE_ENV === 'production';
+    const isTest = process.env.NODE_ENV === 'test';
 
     const openapiFile = await readFile('openapi.yaml', { encoding: 'utf-8' });
     const openapi = yaml.parse(openapiFile);
@@ -22,7 +23,9 @@ export async function addGlobal(app: INestApplication<any>) {
         }),
     );
 
-    app.use(morgan(isProduction ? 'combined' : 'dev'));
+    if (!isTest) {
+        app.use(morgan(isProduction ? 'combined' : 'dev'));
+    }
 
     app.use('/docs', swagger.serve, swagger.setup(openapi, { customSiteTitle: 'Shikicinema API v1' }));
 }
