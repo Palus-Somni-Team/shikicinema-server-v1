87bf4c12a6a270f464af74321e9a8a76cb6cbc96
Smarthard <th3smartchan@gmail.com>
2026-07-09 02:00:38 +0300

    add index to speed up sorting animes by titles

---
+++ src/migrations/1783551551061-add-anime-titles-lang-priority.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddAnimeTitlesLangPriority1783551551061 implements MigrationInterface {
+    name = 'AddAnimeTitlesLangPriority1783551551061'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE INDEX CONCURRENTLY IF NOT EXISTS "idx_anime_titles_anime_lang_priority" ON "anime_titles" ("anime_id", "language", "priority" ASC)`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX IF EXISTS "idx_anime_titles_anime_lang_priority"`);
+    }
+}
