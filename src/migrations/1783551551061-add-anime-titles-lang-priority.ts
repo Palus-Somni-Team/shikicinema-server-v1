461f7b8053e4686bae68a8d3343990ec6e1f165d
Smarthard <th3smartchan@gmail.com>
2026-07-09 02:08:09 +0300

    fix migration

--- src/migrations/1783551551061-add-anime-titles-lang-priority.ts
+++ src/migrations/1783551551061-add-anime-titles-lang-priority.ts
@@ -4,7 +4,7 @@ export class AddAnimeTitlesLangPriority1783551551061 implements MigrationInterfa
     name = 'AddAnimeTitlesLangPriority1783551551061'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE INDEX CONCURRENTLY IF NOT EXISTS "idx_anime_titles_anime_lang_priority" ON "anime_titles" ("anime_id", "language", "priority" ASC)`);
+        await queryRunner.query(`CREATE INDEX IF NOT EXISTS "idx_anime_titles_anime_lang_priority" ON "anime_titles" ("anime_id", "language", "priority" ASC)`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
